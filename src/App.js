57812b9234d1d854efbe70ecc62124623b367062
Reem Tamir <reem.tamir36@gmail.com>
2022-11-20 23:32:38 +0200

    add clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import CountriesList from './components/CountriesList';
 import { useState } from 'react';
+import Clock from './components/Clock';
 
 function App() {
   const [isDark, setIsDark] = useState(false);
@@ -16,13 +17,14 @@ function App() {
         }}
         className="App vh-100% m-auto text-center"
       >
+        {' '}
+        <Clock />
         <h1
           style={{ color: isDark ? 'white' : 'black' }}
           className="text-center"
         >
           Countries
         </h1>
-
         <CountriesList />
         <button onClick={handleClick} className="btn btn-danger mt-4  ">
           {isDark ? 'Day mode' : 'Dark mode'}
---
+++ src/components/Clock.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const Clock = () => {
+  let clockIntervalId;
+  const [hourToShow, setHourToShow] = useState('');
+  clockIntervalId = setInterval(() => {
+    setHourToShow(new Date().toLocaleTimeString());
+  }, 1000);
+
+  return <p className="fs-3 text-danger">{hourToShow}</p>;
+};
+export default Clock;
